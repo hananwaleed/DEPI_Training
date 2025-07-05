b3eb59b59f917e332dbc312a20fd4ce193a2859b
hananwaleed <waleedhanan81@gmail.com>
2025-07-05 12:45:43 +0300

    // Print names using for-in loop

---
+++ 02/Session One/for_in_loop_names.dart
@@ -0,0 +1,9 @@
+/*
+You have a list of names: ["Ali", "Sara", "Omar", "Layla"]
+Use a for-in loop to print each name on a new line. */
+void main() {
+  List<String> names = ["Ali", "Sara", "Omar", "Layla"];
+  for (var name in names) {
+    print(name);
+  }
+}
