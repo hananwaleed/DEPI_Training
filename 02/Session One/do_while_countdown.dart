55db45ef5cc40109dfa9b99d6e42650149fb41e2
hananwaleed <waleedhanan81@gmail.com>
2025-07-05 12:48:34 +0300

    Print numbers from 10 to 1 using do-while loop

---
+++ 02/Session One/do_while_countdown.dart
@@ -0,0 +1,10 @@
+/*
+Write a Dart program using a do-while loop that prints the numbers from 10 down to 1.
+ */
+void main() {
+  int number = 10;
+  do {
+    print(number);
+    number--;
+  } while (number >= 1);
+}
