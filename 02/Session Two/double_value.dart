b70b4b05b1d2117660b3c1e7963580f362f46b75
hananwaleed <waleedhanan81@gmail.com>
2025-07-07 20:17:08 +0300

    Add doubleValue function using arrow syntax

---
+++ 02/Session Two/double_value.dart
@@ -0,0 +1,10 @@
+/*
+Create a function doubleValue(int x) that returns 
+double of x using single-line arrow syntax.
+*/
+
+void main() {
+  print(doubleValue(20));
+}
+
+int doubleValue(int x) => x * 2;
