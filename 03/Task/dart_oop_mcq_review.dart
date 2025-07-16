32757dfe2dcc8ef0ddabdb2c6f1ea42ca276f08a
hananwaleed <waleedhanan81@gmail.com>
2025-07-16 20:10:56 +0300

    Add Dart OOP MCQ answers for review

---
+++ 03/Task/dart_oop_mcq_review.dart
@@ -0,0 +1,17 @@
+/*
+1. B) A blueprint for creating objects
+2. C) Default constructor
+3. C) Ali
+4. D) extends
+5. B) Refers to the parent class
+6. C) It allows code reuse
+7. D) Polymorphism
+8. C) They control access to private variables
+9. A) mixin Logger on Object {}
+10. B) Mixins can have constructors
+11. B) implements copies methods only without code
+12. C) Compiler error
+13. C) To call the parent class constructor
+14. A) Yes
+15. B) Hello from B
+*/
