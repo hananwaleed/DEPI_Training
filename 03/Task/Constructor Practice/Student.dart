1e2ae6ee98f8b0a2d133ad3506ff10657795a35a
hananwaleed <waleedhanan81@gmail.com>
2025-07-16 23:53:30 +0300

    Student class with name and grade using constructor

---
+++ 03/Task/Constructor Practice/Student.dart
@@ -0,0 +1,12 @@
+class Student {
+  String name;
+  double grade;
+
+  //constructor
+  Student(this.name, this.grade);
+
+  void disPlayInfo() {
+    print("Name :$name");
+    print("Grade :$grade");
+  }
+}
---
+++ 03/Task/Constructor Practice/main.dart
@@ -0,0 +1,11 @@
+/*
+Create a class Student with a constructor that accepts name and grade. 
+Use this to assign values. 
+In main(), create and print the student info. 
+ */
+import 'Student.dart';
+
+void main() {
+  Student StuOne = Student("Hanan", 83.9);
+  StuOne.disPlayInfo();
+}
