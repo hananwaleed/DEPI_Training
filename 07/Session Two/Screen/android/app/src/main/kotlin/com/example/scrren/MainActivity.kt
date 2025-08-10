e9fae8d806dfd520911b31f4225c11d2dfe85c58
hananwaleed <waleedhanan81@gmail.com>
2025-08-11 02:08:00 +0300

    create

---
+++ 07/Session Two/Screen/android/app/src/main/kotlin/com/example/scrren/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scrren
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ 07/Session Two/Screen/lib/screens/home_scrren.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScrren extends StatelessWidget {
+  const HomeScrren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
